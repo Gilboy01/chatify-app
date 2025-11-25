268d9dc5ad2ef5c82dd4da55a0e15955b2122cbd
Gilboy01 <gilbertmwega@gmail.com>
2025-11-25 17:07:09 +0300

    updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,9 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+//added this
+import { fileURLToPath } from "url";
+
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -12,7 +15,10 @@ import {ENV} from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
 // const app = express(); we implement socket no longer useful
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //  console.log(process.env.PORT);
 // incase port is not defined, 3000 is the fallback value
@@ -29,12 +35,19 @@ app.use("/api/messages", messageRoutes);
 
 //make ready for deployment
 if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    // app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    const frontendPath = path.join(__dirname, "../../frontend/dist");
+    app.use(express.static(frontendPath));
 
     // any other routes other than those above
-    app.get("*", (req,res) =>{
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-    });
+     app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+
+    
+    // app.get("*", (req,res) =>{
+    //     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    // });
 }
 
 
